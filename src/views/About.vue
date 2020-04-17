9bf225be5460cba6c5218bcc9f94b84bcc3944f2
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-04-17 22:49:33 +0200

    Initial Game Proyect

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Example V-FOR Vue</h1>
+    <div>
+      <ul>
+        <li v-for="(ingrediente, i) in ingredientes" :key="ingrediente.id">{{ingrediente}} - {{i}}</li>
+      </ul>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      ingredientes: ['Lionel', 'Mateo', 'Andrea']
+    }
+  }
+}
+</script>
---
+++ src/views/Game.vue
@@ -0,0 +1,169 @@
+<template>
+    <div>
+        <section class="row">
+            <div class="small-6 columns">
+                <h1 class="text-center">YOU</h1>
+                <div class="healthbar">
+                    <div class="healthbar text-center" 
+                         style="background-color: green; margin: 0; color: white;"
+                         :style="{width: playerHealth + '%'}">
+                        {{playerHealth}}
+                    </div>
+                </div>
+            </div>
+            <div class="small-6 columns">
+                <h1 class="text-center">MONSTER</h1>
+                <div class="healthbar">
+                    <div class="healthbar text-center" 
+                         style="background-color: green; margin: 0; color: white;"
+                         :style="{width: monsterHealth + '%'}">
+                        {{monsterHealth}}
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="row controls"
+                 v-if="!gameIsRunning">
+            <div class="small-12 columns">
+                <button id="start-game"
+                        @click="startGame">
+                        START NEW GAME
+                </button>
+            </div>
+        </section>
+        <section v-else 
+                 class="row controls">
+            <div class="small-12 columns">
+                <button id="attack">ATTACK</button>
+                <button id="special-attack">SPECIAL ATTACK</button>
+                <button id="heal">HEAL</button>
+                <button id="give-up">GIVE UP</button>
+            </div>
+        </section>
+        <section class="row log">
+            <div class="small-12 columns">
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                playerHealth: 10,
+                monsterHealth: 80,
+                gameIsRunning: false
+            }
+        },
+        methods: {
+            startGame: function() {
+                this.gameIsRunning = true;
+                this.playerHealth = 100,
+                this.monsterHealth = 100
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .text-center {
+        text-align: center;
+    }
+
+    .healthbar {
+        width: 80%;
+        height: 40px;
+        background-color: #eee;
+        margin: auto;
+        transition: width 500ms;
+    }
+
+    .controls, .log {
+        margin-top: 30px;
+        text-align: center;
+        padding: 10px;
+        border: 1px solid #ccc;
+        box-shadow: 0px 3px 6px #ccc;
+    }
+
+    .turn {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        font-weight: bold;
+        font-size: 22px;
+    }
+
+    .log ul {
+        list-style: none;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+
+    .log ul li {
+        margin: 5px;
+    }
+
+    .log ul .player-turn {
+        color: blue;
+        background-color: #e4e8ff;
+    }
+
+    .log ul .monster-turn {
+        color: red;
+        background-color: #ffc0c1;
+    }
+
+    button {
+        font-size: 20px;
+        background-color: #eee;
+        padding: 12px;
+        box-shadow: 0 1px 1px black;
+        margin: 10px;
+    }
+
+    #start-game {
+        background-color: #aaffb0;
+    }
+
+    #start-game:hover {
+        background-color: #76ff7e;
+    }
+
+    #attack {
+        background-color: #ff7367;
+    }
+
+    #attack:hover {
+        background-color: #ff3f43;
+    }
+
+    #special-attack {
+        background-color: #ffaf4f;
+    }
+
+    #special-attack:hover {
+        background-color: #ff9a2b;
+    }
+
+    #heal {
+        background-color: #aaffb0;
+    }
+
+    #heal:hover {
+        background-color: #76ff7e;
+    }
+
+    #give-up {
+        background-color: #ffffff;
+    }
+
+    #give-up:hover {
+        background-color: #c7c7c7;
+    }
+</style>
